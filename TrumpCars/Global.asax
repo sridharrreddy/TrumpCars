5beaca9c06e77d168faedaa6c7616014d38ddddf
Sridhar Reddy <srreddy@bauermedia.co.nz>
2016-08-29 10:56:27 +1200

    Skeleton

---
+++ TrumpCars/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrumpCars.MvcApplication" Language="C#" %>
---
+++ TrumpCars/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TrumpCars.Startup))]
+
+namespace TrumpCars
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
