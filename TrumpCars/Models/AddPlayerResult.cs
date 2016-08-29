4fd1b69851716847d077cfcbf23eb54f757f1e4b
Sridhar Reddy <srreddy@bauermedia.co.nz>
2016-08-29 17:23:10 +1200

    Joining a group - still to fix some issues

---
+++ TrumpCars/Models/AddPlayerResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrumpCars.Models
+{
+    public class AddPlayerResult
+    {
+        public bool IsGroupFull { get; set; }
+        public string GroupName { get; set; }
+    }
+}
